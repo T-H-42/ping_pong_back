ff459f10760704f86d1fbb7cbf5fbaa40007aa20
Namik Hwang <nhwang@c5r1s4.42seoul.kr>
2023-07-31 12:41:54 +0900

    docs: seperate Modules and gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
-import { FriendModule } from './friend/friend.module';
+import { FriendModule } from './modules/friend.module';
 // import { FriendGatewayGateway } from './friend-gateway/friend-gateway.gateway';
 // import { FriendGatewayModule } from './friend-gateway/friend-gateway.module';
 // import { ChatGateway } from './chat/chat.gateway';
-import { ChatModule } from './chat/chat.module';
-import { PingPongModule } from './ping_pong/ping_pong.module';
-import { ChatMuteModule } from './chat_mute/chat_mute.module';
-import { ChatBlockModule } from './chat_block/chat_block.module';
-import { ChatUserModule } from './chat_user/chat_user.module';
-import { GameModule } from './game/game.module';
+import { ChatModule } from './modules/chat.module';
+import { PingPongModule } from './modules/ping_pong.module';
+import { ChatMuteModule } from './modules/chat_mute.module';
+import { ChatBlockModule } from './modules/chat_block.module';
+import { ChatUserModule } from './modules/chat_user.module';
+import { GameModule } from './modules/game.module';
 
 @Module({
   imports: [
