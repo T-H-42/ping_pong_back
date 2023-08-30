c1b295f9e8061b2b81c4c05b032a35d39c82fd0c
hyna <alexisna77@gmail.com>
2023-08-30 16:56:53 +0900

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -457,7 +457,7 @@ export class UserService {
       username,
       image.filename,
     );
-    if (!imageUpdate.affected) throw new InternalServerErrorException();
+    if (!imageUpdate.affected) throw new UnauthorizedException();
     return 'succeed';
   }
 
